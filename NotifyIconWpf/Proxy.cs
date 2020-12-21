f44fc0d7eb1e89eb02d897b1d4f5b6a5a1caa154
Sam Grogan <sam@fusionmedia.com>
2020-12-21 08:47:51 -0800

    Add icons to sample, project setup

---
+++ NotifyIconWpf/Proxy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NotifyIcon;
+
+namespace NotifyIcon.Wpf
+{
+    public partial class Proxy : FrameworkElement, IDisposable
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
