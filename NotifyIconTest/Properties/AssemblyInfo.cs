db10c238f221a0861086adf3b9d66721f62c45cf
Spencer Voorhees <81316326+stvoorhees@users.noreply.github.com>
2025-03-04 09:26:04 -0800

    Assembly info restoration

---
+++ NotifyIconTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NotifyIconTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sam Grogan")]
+[assembly: AssemblyProduct("NotifyIconTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("872d6df4-bf1b-423f-8511-7ba5e9749fdb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
